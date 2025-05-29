e77af1fca564cac1f464d0c97654d639afb832f7
Aleks Rūtiņš <keeper@farthergate.com>
2025-05-29 17:18:37 -0400

    Manually pass token

--- mod.ts
+++ mod.ts
@@ -5,15 +5,20 @@ export const Job =
     project,
     service,
     environment,
+    token,
   }: {
     project?: string;
     service?: string;
     environment?: string;
+    token?: string;
   } = {}): (() => void) =>
   async () => {
     const railway = "./node_modules/.bin/railway";
-    if (project) await run(`${railway} link -p "${project}"`);
-    await run(`${railway} up -c -s "${service}" -e "${environment}"`);
+    if (project)
+      await run(`RAILWAY_TOKEN=${token} ${railway} link -p "${project}"`);
+    await run(
+      `RAILWAY_TOKEN=${token} ${railway} up -c -s "${service}" -e "${environment}"`,
+    );
     annotate({
       icon: Icons.App,
       color: "fg",
